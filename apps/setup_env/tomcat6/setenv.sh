2ed5ed7e3653cbf7b10a21f6f9625dcb6ec6695b
secretter <secretter@sec.com>
2017-05-14 17:54:07 +0900

    F

---
+++ apps/setup_env/tomcat6/setenv.sh
@@ -0,0 +1,4 @@
+export JAVA_OPTS="-Dcom.sun.management.jmxremote=true \
+-Dcom.sun.management.jmxremote.ssl=false \
+-Dcom.sun.management.jmxremote.authenticate=false \
+-Djava.rmi.server.hostname=192.168.33.10"
